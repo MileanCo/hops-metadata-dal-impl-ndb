82371a3f8d35af5468ab37844930364759765705
Joel Stenkvist <joelm.sten@gmail.com>
2019-03-11 13:27:27 -0700

    added code for listChildren, put, and get methods for ndb

--- src/main/java/io/hops/metadata/ndb/dalimpl/s3/S3MetadataClusterj.java
+++ src/main/java/io/hops/metadata/ndb/dalimpl/s3/S3MetadataClusterj.java
@@ -1,15 +1,67 @@
 package io.hops.metadata.ndb.dalimpl.s3;
 
+import com.mysql.clusterj.annotation.Column;
+import com.mysql.clusterj.annotation.PersistenceCapable;
+import com.mysql.clusterj.annotation.PrimaryKey;
+import io.hops.exception.StorageException;
 import io.hops.metadata.ndb.ClusterjConnector;
+import io.hops.metadata.ndb.wrapper.*;
 import io.hops.metadata.s3.TablesDef;
 import io.hops.metadata.s3.dal.S3MetadataAccess;
 import io.hops.metadata.s3.entity.S3PathMetadata;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class S3MetadataClusterj implements TablesDef.S3PathMetadataTableDef, S3MetadataAccess<S3PathMetadata> {
 
     private ClusterjConnector connector = ClusterjConnector.getInstance();
+
+    @PersistenceCapable(table = TABLE_NAME)
+    public interface S3PathMetadataDTO {
+        @PrimaryKey
+        @Column(name = PARENT)
+        String getParent();
+        void setParent(String parent);
+
+        @PrimaryKey
+        @Column(name = CHILD)
+        String getChild();
+        void setChild(String child);
+
+        @PrimaryKey
+        @Column(name = BUCKET)
+        String getBucket();
+        void setBucket(String bucket);
+
+        @Column(name = IS_DELETED)
+        Boolean getIsDeleted();
+        void setIsDeleted(Boolean deleted);
+
+        @Column(name = BLOCK_SIZE)
+        Long getBlockSize();
+        void setBlockSize(Long blockSize);
+
+        @Column(name = FILE_LENGTH)
+        Long getFileLength();
+        void setFileLength(Long fileLength);
+
+        @Column(name = MOD_TIME)
+        Long getModTime();
+        void setModTime(Long modTime);
+
+        @Column(name = IS_DIR)
+        Boolean getIsDir();
+        void setIsDir(Boolean isDir);
+
+        @Column(name = TABLE_CREATED)
+        Long getTableCreated();
+        void setTableCreated(Long tableCreated);
+
+        @Column(name = TABLE_VERSION)
+        Long getTableVersion();
+        void setTableVersion(Long tableVersion);
+    }
 
     @Override
     public S3PathMetadata getPath(String parent, String child) {
@@ -17,8 +69,27 @@ public class S3MetadataClusterj implements TablesDef.S3PathMetadataTableDef, S3M
     }
 
     @Override
-    public boolean putPath(S3PathMetadata path) {
-        return false;
+    public boolean putPath(S3PathMetadata path) throws StorageException {
+        HopsSession session = connector.obtainSession();
+        S3PathMetadataDTO dto = null;
+        // TODO: check if this path already exists? INSERT OR UPDATE?
+        try {
+            dto = session.newInstance(S3PathMetadataDTO.class);
+
+            dto.setParent(path.getParent());
+            dto.setChild(path.getChild());
+            dto.setBucket(path.getBucket());
+            dto.setBlockSize(path.getBlockSize());
+            dto.setFileLength(path.getFileLength());
+            dto.setIsDeleted(path.isDeleted());
+            dto.setIsDir(path.isDir());
+            dto.setModTime(path.getModTime());
+
+            session.makePersistent(dto);
+        } finally {
+            session.release(dto);
+        }
+        return true;
     }
 
     @Override
@@ -27,18 +98,45 @@ public class S3MetadataClusterj implements TablesDef.S3PathMetadataTableDef, S3M
     }
 
     @Override
-    public boolean putPaths(List<S3PathMetadata> paths) {
+    public boolean putPaths(List<S3PathMetadata> paths)throws StorageException  {
         return false;
     }
 
     @Override
-    public boolean deletePaths(List<List<String>> paths) {
+    public boolean deletePaths(List<List<String>> paths)throws StorageException  {
         return false;
     }
 
     @Override
-    public boolean isDirEmpty(String parent, String child) {
-        return false;
+    public boolean isDirEmpty(String parent, String child) throws StorageException {
+        String dir_key = parent + '/' + child;
+        List<S3PathMetadata> dir_children = getPathChildren(dir_key);
+        return dir_children.isEmpty();
+    }
+
+    /**
+     * Convert a list of S3MetadataDTO's into storages
+     */
+    private List<S3PathMetadata> convertAndRelease(HopsSession session, List<S3PathMetadataDTO> dtos) throws StorageException {
+        ArrayList<S3PathMetadata> list = new ArrayList(dtos.size());
+
+        for (S3PathMetadataDTO dto : dtos) {
+            S3PathMetadata path = new S3PathMetadata();
+            path.parent = dto.getParent();
+            path.child = dto.getChild();
+            path.isDeleted = dto.getIsDeleted();
+            path.blockSize = dto.getBlockSize();
+            path.fileLength = dto.getFileLength();
+            path.modTime = dto.getModTime();
+            path.isDir = dto.getIsDir();
+            path.tableCreated = dto.getTableCreated();
+            path.tableVersion = dto.getTableVersion();
+
+            list.add(path);
+            session.release(dto);
+        }
+
+        return list;
     }
 
     @Override
@@ -46,14 +144,25 @@ public class S3MetadataClusterj implements TablesDef.S3PathMetadataTableDef, S3M
         return false;
     }
 
-
     @Override
     public List<S3PathMetadata> getExpiredFiles(long modTime) {
         return null;
     }
 
     @Override
-    public List<S3PathMetadata> getPathChildren(String parent) {
-        return null;
+    public List<S3PathMetadata> getPathChildren(String parent) throws StorageException {
+        HopsSession session = connector.obtainSession();
+        HopsQueryBuilder qb = session.getQueryBuilder();
+
+        // build the SQL query
+        HopsQueryDomainType<S3PathMetadataDTO> dobj = qb.createQueryDefinition(S3PathMetadataDTO.class);
+        HopsPredicate pred1 = dobj.get("parent").equal(dobj.param("parent"));
+        dobj.where(pred1);
+
+        // Set the query search parameters
+        HopsQuery<S3PathMetadataDTO> query = session.createQuery(dobj);
+        query.setParameter("parent", parent);
+
+        return convertAndRelease(session, query.getResultList());
     }
 }
